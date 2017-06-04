e4051128920dbf1d7786ecde4582fd2294c69729
surendra <surendrameena5@gmail.com>
2017-06-05 01:02:30 +0530

    specific email allowed to register

--- reg/forms.py
+++ reg/forms.py
@@ -7,6 +7,15 @@ from reg.models import UserProfile
 class UserForm(forms.ModelForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform avalid email address.')
     password = forms.CharField(widget=forms.PasswordInput())
+
+    allowed = ['itbhu.ac.in', 'iitbhu.ac.in']
+    def clean_email(self):
+
+        data = self.cleaned_data['email'].split('@')[-1]
+        if data not in self.allowed:   # any check you need
+            raise forms.ValidationError("Must be a gmail address")
+        return self.cleaned_data['email']
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'first_name', 'last_name')
